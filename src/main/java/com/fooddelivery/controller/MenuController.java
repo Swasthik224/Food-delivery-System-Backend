e5ecef9c6cfcac540f03be0f1a6f7ecd0c2afe0a
Manju swasthik <155383159+Swasthik224@users.noreply.github.com>
2026-05-19 22:12:14 +0530

    Update MenuController.java

--- src/main/java/com/fooddelivery/controller/MenuController.java
+++ src/main/java/com/fooddelivery/controller/MenuController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:3000","https://food-delivery-system-inky.vercel.app")
 public class MenuController {
 
     private final MenuItemRepository menuItemRepository;
