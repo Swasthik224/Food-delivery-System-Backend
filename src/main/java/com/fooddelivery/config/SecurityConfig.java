1c5b8caa22e8917d54eb6ee10476b5b21c663b76
Manju swasthik <155383159+Swasthik224@users.noreply.github.com>
2026-05-19 22:32:47 +0530

    Update SecurityConfig.java

--- src/main/java/com/fooddelivery/config/SecurityConfig.java
+++ src/main/java/com/fooddelivery/config/SecurityConfig.java
@@ -82,7 +82,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173","https://food-delivery-system-inky.vercel.app"));
+        config.setAllowedOriginPatterns(List.of(
+                "http://localhost:5173",
+                "https://food-delivery-system-inky.vercel.app"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
