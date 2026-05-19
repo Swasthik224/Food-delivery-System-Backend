a81cf9a797712242e1b8c6cbc609f5303b74df54
Manju swasthik <155383159+Swasthik224@users.noreply.github.com>
2026-05-19 22:11:41 +0530

    Update AuthController.java

--- src/main/java/com/fooddelivery/controller/AuthController.java
+++ src/main/java/com/fooddelivery/controller/AuthController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:3000","https://food-delivery-system-inky.vercel.app")
 public class AuthController {
 
     private final AuthService authService;
